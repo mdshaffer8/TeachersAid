0038d9f1c3b01c067bc173e5dccd08559c731775
mdshaffer8 <mdshaffer8@live.com>
2018-05-07 13:46:22 -0400

    update database

--- app/models.py
+++ app/models.py
@@ -6,5 +6,8 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
 
 
