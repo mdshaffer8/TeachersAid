6cae0a551e581765b0cdbeab3090d98d99d938b1
mdshaffer8 <mdshaffer8@live.com>
2018-05-30 14:21:00 -0400

    new database set up

--- teachersaid/__init__.py
+++ teachersaid/__init__.py
@@ -1,18 +1,18 @@
-import os
-from os.path import join, isfile
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_uploads import UploadSet, IMAGES, configure_uploads
+from flask_migrate import Migrate
+
+from config import Config
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 
 bcrypt = Bcrypt(app)
 
@@ -20,12 +20,7 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
-from teachersaid import routes
+from teachersaid import routes, models
